e90a00dd1343af6173382372c7ec7b2aa507d1e3
nek07 <abylaiooaooa@mail.ru>
2023-11-18 12:56:54 +0600

    not final strategy pattern

---
+++ src/strategy/CashPayment.java
@@ -0,0 +1,8 @@
+package strategy;
+
+class CashPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " in cash");
+    }
+}
---
+++ src/strategy/ElectronicStore.java
@@ -0,0 +1,18 @@
+package strategy;
+
+class ElectronicStore {
+    private PaymentStrategy paymentStrategy;
+
+    public ElectronicStore(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void processOrder(double amount) {
+        System.out.println("Processing order...");
+        paymentStrategy.pay(amount);
+    }
+}
